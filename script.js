988ae4243a69dac66e0d94dd2a31038fba0571bb
code3110 <code3110.mail@gmail.com>
2019-02-23 19:31:43 +0900

    _

---
+++ script.js
@@ -0,0 +1,14 @@
+// let nicoRes;
+// $(function () {
+//     $.ajax({
+//         url: 'http://seiga.nicovideo.jp/api/user/data?id=8812193',
+//         type: 'GET',
+//         dataType: 'xml',
+//         timeout: 1000,
+//         crossDomain: true
+//     }).then(function (data) {
+//         $(data).find("image").each(function () {
+//             console.log($(this).find("id").text);
+//         })
+//     })
+// });
